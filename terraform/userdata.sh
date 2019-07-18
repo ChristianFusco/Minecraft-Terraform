7a6b39194449ce4d4ef6e9c9680ae3ded57ec3b8
Christian Fusco <christian.fusco96@gmail.com>
2019-07-17 17:50:49 -0700

    eula multiline didnt work.

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,12 +5,11 @@ cd /minecraft
 
 wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
 
-EULA=$(cat <<-END
-#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).\n
-#Tue Jul 16 19:33:18 PDT 2019\n
-eula=true\n
-END
-)
+read -r -d '' EULA << EOM
+#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Tue Jul 16 19:33:18 PDT 2019
+eula=true
+EOM
 
 echo $EULA > eula.txt
 
