c1c8b8c6191dd0f7564800b5b5011b8a4d105cce
ChristianFusco <Christian.Fusco@mymail.champlain.edu>
2019-07-28 11:49:02 -0700

    updated java opts, delete ssm.

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Hogs a bunch of memory and it's not even set up right now
+yum erase amazon-ssm-agent –y
+
 mkdir /minecraft
 cd /minecraft
 
@@ -13,4 +16,10 @@ echo "eula=true" > eula.txt
 
 yum -y install java-1.8.0-openjdk
 
-exec java -Xmx1024M -Xms1024M -jar server.jar nogui &
+exec java -server -Xmx768M -Xms768M \
+	-XX:+UseParNewGC \
+  	-XX:ParallelGCThreads=2 \
+  	-XX:+UseCompressedOops \
+  	-XX:+TieredCompilation \
+  	-XX:+DoEscapeAnalysis \
+  	-jar server.jar nogui &
