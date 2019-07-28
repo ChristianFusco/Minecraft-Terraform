ce9aac6bdb0a3b3ece9bfbe95e81c80e790670e8
TatianaPr <tatianaprincivil@gmail.com>
2019-07-27 22:10:47 -0400

    terraform installs should be more consistent now.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,13 @@
-sudo apt install linuxbrew-wrapper
 
-# deploys 
 
-brew install terraform
+
+# terraform 
+terraform_version=0.12.5
+sudo apt-get install unzip
+wget https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip
+unzip terraform_${terraform_version}_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+rm terraform_${terraform_version}_linux_amd64.zip
 
 # aws cli
 
