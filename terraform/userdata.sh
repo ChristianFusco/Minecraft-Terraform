25f39aaae2b98677290faa1dcd8f7741d0e6e934
ChristianFusco <Christian.Fusco@mymail.champlain.edu>
2019-07-28 09:01:46 -0700

    mem fix, always get latest minecraft.

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -3,10 +3,14 @@
 mkdir /minecraft
 cd /minecraft
 
-wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
+# Probably don't need the && there to get the minecraft latest name
+url=$(curl https://www.minecraft.net/en-us/download/server/ | grep minecraft_server | cut -d \" -f2) \
+ && url=$(echo $url | cut -d ' ' -f1)
+
+wget $url
 
 echo "eula=true" > eula.txt
 
 yum -y install java-1.8.0-openjdk
 
-exec java -Xmx512M -Xms512M -jar server.jar nogui &
+exec java -Xmx1024M -Xms1024M -jar server.jar nogui &
