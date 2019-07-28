cfdeb7acaa803d2f170df91f8f555c92f85e19ce
ChristianFusco <Christian.Fusco@mymail.champlain.edu>
2019-07-28 07:09:58 -0700

    update eula text. newlines are hard.

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,13 +5,7 @@ cd /minecraft
 
 wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
 
-read -r -d '' EULA << EOM
-#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
-#Tue Jul 16 19:33:18 PDT 2019
-eula=true
-EOM
-
-echo $EULA > eula.txt
+echo "eula=true" > eula.txt
 
 yum -y install java-1.8.0-openjdk
 
